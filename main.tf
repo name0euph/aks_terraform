f6b19ade73db3ce59132cd79b59d739543cc977b
name0euph <135337772+name0euph@users.noreply.github.com>
2024-06-03 01:18:47 +0000

    APIMを追加

--- main.tf
+++ main.tf
@@ -32,3 +32,13 @@ resource "azurerm_resource_group" "rg" {
 
     tags = var.tag
 }
+
+resource "azurerm_api_management" "apim" {
+  name                = "apim-cft-openai-arisaka"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+  publisher_name      = "Ryota Arisaka"
+  publisher_email     = "ryouta-arisaka@jfe-systems.com"
+
+  sku_name = "Developer_1"
+}
