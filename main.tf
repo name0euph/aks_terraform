b608579e09f00d81ee8425f42c8bce613eddc2b6
name0euph <135337772+name0euph@users.noreply.github.com>
2024-06-07 01:44:47 +0000

    Update azurerm_cognitive_account and azurerm_cognitive_deployment configurations in main.tf

--- main.tf
+++ main.tf
@@ -48,6 +48,10 @@ resource "azurerm_api_management" "apim" {
   publisher_email     = "ryouta-arisaka@jfe-systems.com"
   sku_name            = "Consumption_0"
 
+  identity {
+    type         = "SystemAssigned"
+  }
+
   tags = var.tag
 }
 
@@ -55,6 +59,7 @@ resource "azurerm_cognitive_account" "openai" {
   name                = "aoai-cft-openai-arisaka"
   resource_group_name = azurerm_resource_group.rg.name
   location            = "West US"
+  custom_subdomain_name = "aoai-cft-openai-arisaka"
   kind                = "OpenAI"
   sku_name            = "S0"
 
@@ -87,6 +92,25 @@ resource "azurerm_cognitive_deployment" "gpt-4o" {
     format  = "OpenAI"
     name    = "gpt-4o"
     version = "2024-05-13"
+  }
+  scale {
+    capacity = "10"
+    type     = "Standard"
+  }
+  rai_policy_name = "Microsoft.Default"
+
+  depends_on = [
+    azurerm_cognitive_account.openai,
+  ]
+}
+
+resource "azurerm_cognitive_deployment" "gpt-4" {
+  name                 = "gpt-4"
+  cognitive_account_id = azurerm_cognitive_account.openai.id
+  model {
+    format  = "OpenAI"
+    name    = "gpt-4"
+    version = "1106-Preview"
   }
   scale {
     capacity = "10"
