57d0a5387802e1af24e64ffa380d86d3eb47092c
name0euph <135337772+name0euph@users.noreply.github.com>
2024-06-22 15:53:43 +0000

    chore: Update dependencies for JWT and Echo

--- go/main.go
+++ go/main.go
@@ -12,9 +12,12 @@ func main() {
 	db := db.NewDB()
 	// 依存性の注入
 	userRepository := repository.NewUserRepository(db)
+	taskRepository := repository.NewTaskRepository(db)
 	userUsecase := usecase.NewUserUsecase(userRepository)
+	taskUsecase := usecase.NewTaskUsecase(taskRepository)
 	userController := controller.NewUserController(userUsecase)
-	e := router.NewRouter(userController)
+	taskController := controller.NewTaskController(taskUsecase)
+	e := router.NewRouter(userController, taskController)
 
 	// サーバー起動
 	e.Logger.Fatal(e.Start(":8080"))
