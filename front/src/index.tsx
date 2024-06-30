ff8081315caadc4fbaf6922c615e14e0b0abfdf1
name0euph <name0euph@gmail.com>
2024-06-30 13:39:33 +0000

    chore: Add QueryClientProvider for global query usage

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -3,13 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+const queryClient = new QueryClient({});
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+// QueryClientProvider でラップすることで、全てのコンポーネントでクエリを利用できる
 root.render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+      <ReactQueryDevtools initialIsOpen={false}/>
+    </QueryClientProvider>
   </React.StrictMode>
 );
 
