59a0e4813b1f991cc577d6bd952a4b04aa088f13
name0euph <name0euph@gmail.com>
2024-07-04 11:52:05 +0000

    add: base react app

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,25 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Auth } from './components/Auth';
+import { Todo } from './components/Todo';
+import axios from 'axios';
+import { CsrfToken } from './types';
 
 function App() {
+  useEffect(() => {
+    axios.defaults.withCredentials = true;
+
+    const getCsrfToken = async () => {
+      // CSRF トークンを取得するエンドポイントにアクセス
+      const { data } = await axios.get<CsrfToken>(
+        `${process.env.REACT_APP_API_URL}/csrf`
+      );
+      // CSRF トークンをリクエストヘッダーに設定
+      axios.defaults.headers.common['X-CSRF-Token'] = data.csrf_token;
+    } 
+    getCsrfToken();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Auth />} />
+        <Route path="/todo" element={<Todo />} />
+      </Routes>
+    </Router>
   );
 }
 
