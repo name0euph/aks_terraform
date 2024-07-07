68a6cfa8bf35c30b1b1e3159829b095ca8bd56db
name0euph <135337772+name0euph@users.noreply.github.com>
2024-07-07 03:32:18 +0000

    test: planがfailするように変更

--- terraform/main.tf
+++ terraform/main.tf
@@ -106,7 +106,7 @@ resource "azurerm_postgresql_flexible_server" "pg" {
 ################################################################
 
 resource "azurerm_container_registry" "acr" {
-  name                = "acrcftopenaiarisaka"
+  name                = "acr-cftopenaiarisaka"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   sku                 = "Basic"
